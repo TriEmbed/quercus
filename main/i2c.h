4c81787627dc3d1da867643627b56a8813ebabd8
Pete Soper <pete@soper.us>
2022-06-27 13:59:54 -0400

    main/i2c.h externs

--- main/i2c.h
+++ main/i2c.h
@@ -7,5 +7,5 @@ cJSON *
 i2cget (uint8_t chip_addr, uint8_t data_addr, uint8_t len);
 cJSON *
 i2cScan ();
-uint8_t i2c_gpio_scl ;
-uint8_t i2c_gpio_sda ;
+extern uint8_t i2c_gpio_scl ;
+extern uint8_t i2c_gpio_sda ;
